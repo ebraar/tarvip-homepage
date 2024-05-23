48a6257cb0ba6fd5a6aed3323e1637fb9165773a
ebraar <ebbet468@gmail.com>
2024-05-23 17:55:12 +0300

    process mobili için ayarlama

--- tarvip-homepage/app/page.js
+++ tarvip-homepage/app/page.js
@@ -1,8 +1,27 @@
+"use client"
 import ProcessSteps from "/components/process";
 import HomeSection from "/components/homepage";
+import MobileProcessSteps from "/components/processMobil";
 import Head from 'next/head';
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
     <Head>
@@ -10,7 +29,9 @@ export default function Home() {
       </Head>
     <div>
       <HomeSection/>
-      <ProcessSteps/>
+      <div>
+      {isMobile ? <MobileProcessSteps /> : <ProcessSteps />}
+      </div>
     </div>
     </>
   );
