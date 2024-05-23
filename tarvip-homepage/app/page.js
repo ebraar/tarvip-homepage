35b2afe29026bb6d530ce592dd3c9887e5addf96
ebraar <ebbet468@gmail.com>
2024-05-23 11:31:15 +0300

    page

---
+++ tarvip-homepage/app/page.js
@@ -0,0 +1,11 @@
+import ProcessSteps from "@/components/process";
+import HomeSection from "/components/homepage";
+
+export default function Home() {
+  return (
+    <div>
+      <HomeSection/>
+      <ProcessSteps/>
+    </div>
+  );
+}
