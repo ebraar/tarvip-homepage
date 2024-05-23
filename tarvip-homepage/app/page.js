27edd26cfae41926cb655e9a8c089775d9d37756
ebraar <ebbet468@gmail.com>
2024-05-23 16:16:55 +0300

    font değişikliği

--- tarvip-homepage/app/page.js
+++ tarvip-homepage/app/page.js
@@ -1,11 +1,17 @@
 import ProcessSteps from "/components/process";
 import HomeSection from "/components/homepage";
+import Head from 'next/head';
 
 export default function Home() {
   return (
+    <>
+    <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
     <div>
       <HomeSection/>
       <ProcessSteps/>
     </div>
+    </>
   );
 }
